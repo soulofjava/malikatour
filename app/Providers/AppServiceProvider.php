998562aaf13bae83d29a0af94f5ba33c8d5c4d46
isamaulanatantra <isamaulanatantra@gmail.com>
2025-05-30 11:18:08 +0700

    app service error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Carbon\Carbon;
 use App\Models\Visitor;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,29 +25,40 @@ class AppServiceProvider extends ServiceProvider
         // if (env('APP_ENV') != 'local') {
         //     $this->app['request']->server->set('HTTPS', true);
         // }
-        // Statistik waktu
-        $today = Carbon::today();
-        $yesterday = Carbon::yesterday();
-        $startOfWeek = Carbon::now()->startOfWeek();
-        $startOfMonth = Carbon::now()->startOfMonth();
-        $startOfYear = Carbon::now()->startOfYear();
+        
+        // Cegah error saat migrate fresh atau saat tabel belum dibuat
+        if (Schema::hasTable('visitors')) {
+            $today = Carbon::today();
+            $yesterday = Carbon::yesterday();
+            $startOfWeek = Carbon::now()->startOfWeek();
+            $startOfMonth = Carbon::now()->startOfMonth();
+            $startOfYear = Carbon::now()->startOfYear();
 
-        // Query
-        $statToday = Visitor::whereDate('visited_at', $today)->count();
-        $statYesterday = Visitor::whereDate('visited_at', $yesterday)->count();
-        $statWeek = Visitor::whereBetween('visited_at', [$startOfWeek, now()])->count();
-        $statMonth = Visitor::whereBetween('visited_at', [$startOfMonth, now()])->count();
-        $statYear = Visitor::whereBetween('visited_at', [$startOfYear, now()])->count();
-        $statTotal = Visitor::count();
+            $statToday = Visitor::whereDate('visited_at', $today)->count();
+            $statYesterday = Visitor::whereDate('visited_at', $yesterday)->count();
+            $statWeek = Visitor::whereBetween('visited_at', [$startOfWeek, now()])->count();
+            $statMonth = Visitor::whereBetween('visited_at', [$startOfMonth, now()])->count();
+            $statYear = Visitor::whereBetween('visited_at', [$startOfYear, now()])->count();
+            $statTotal = Visitor::count();
 
-        // Share ke semua view
-        view()->share('visitorStats', [
-            'today' => $statToday,
-            'yesterday' => $statYesterday,
-            'week' => $statWeek,
-            'month' => $statMonth,
-            'year' => $statYear,
-            'total' => $statTotal,
-        ]);
+            view()->share('visitorStats', [
+                'today' => $statToday,
+                'yesterday' => $statYesterday,
+                'week' => $statWeek,
+                'month' => $statMonth,
+                'year' => $statYear,
+                'total' => $statTotal,
+            ]);
+        } else {
+            // Share data kosong agar tidak error di view saat belum ada tabel
+            view()->share('visitorStats', [
+                'today' => 0,
+                'yesterday' => 0,
+                'week' => 0,
+                'month' => 0,
+                'year' => 0,
+                'total' => 0,
+            ]);
+        }
     }
 }
